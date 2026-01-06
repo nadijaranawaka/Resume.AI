c3ba70c29c872ea085d9450d3f98d801175317ea
nadijaranawaka <nadijaranawaka2727@gmail.com>
2026-01-06 20:12:29 +0530

    Home page decoration and Navbar setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
 import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resume} from "react-dom/server";
+import {resumes} from "~/routes/constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -6,13 +9,18 @@ export function meta({}: Route.MetaArgs) {
     { name: "description", content: "AI-powered contextual resume feedback" },
   ];
 }
-
 export default function Home() {
-    return <main>
+    return <main className= "bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
         <section className="main-section">
             <div className="page-heading">
                 <h1>Track Your Applications & Resume Ratings</h1>
+                <h2>Submit and get AI-powered feedback</h2>
             </div>
         </section>
+        {resumes.map((resume) =>
+        <div>
+            <h1>{resume.jobTitle}</h1>
+        </div>)}
     </main>;
 }
